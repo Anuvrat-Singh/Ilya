3de76c259b07677b597812125174f17f65c075c3
Anuvrat-Singh <s.anuvrat@1tab.com>
2020-05-18 14:14:26 +0530

    Final Changes Done

--- src/test/java/com/ebot7/tests/Task3.java
+++ src/test/java/com/ebot7/tests/Task3.java
@@ -1,25 +1,26 @@
 package com.ebot7.tests;
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
+import org.apache.log4j.Logger;
 import java.util.concurrent.TimeUnit;
 
 public class Task3 {
     WebDriver driver = null;
+    Logger log = Logger.getLogger("Task3.class");
 
     @BeforeMethod
     public void setup() {
         System.out.println("-------------Task 4------------");
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver = new ChromeDriver();
-
+        log.info("Browser is launched");
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
 
         driver.get("https://www-5eb9089a8e101d4880a5fbda.recruit.eb7.io/");
+        log.info("Opening URL");
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 
     }
@@ -38,6 +39,7 @@ public class Task3 {
             driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click();
         }
         Assert.assertNotEquals(i,1, "Second Iteration passed with exact same input. Plagiarism check not implemented");
+        log.info("Plagiarism check not implemented");
     }
 
     @Test(priority = 2)
@@ -46,14 +48,14 @@ public class Task3 {
             String str = "123";
             System.out.println("The length of the quote should be between 10 and 200 characters.");
             System.out.println("Length of input string =" + str.length());
-
+            log.info("Testing minimum length");
             driver.findElement(By.xpath("//button[contains(text(),'Add new quote')]")).click();
             driver.findElement(By.xpath("//input[@id=\"autorInput\"]"))
                     .sendKeys("Reid");
             driver.findElement(By.xpath("//input[@id=\"quoteInput\"]"))
                     .sendKeys(str);
             driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click();
-
+            log.info("Minimum length validation has failed");
             System.out.println("Still in try block. No exception raised.");
         }
         catch (Exception e){
@@ -64,6 +66,7 @@ public class Task3 {
 
     @AfterMethod
     public void tearDown(){
+        log.info("Closing the browser");
         driver.close();
     }
 
--- src/test/java/com/ebot7/tests/Task4.java
+++ src/test/java/com/ebot7/tests/Task4.java
@@ -1,27 +1,29 @@
 package com.ebot7.tests;
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Logger;
 
 public class Task4 {
 
     WebDriver driver = null;
+    Logger log = Logger.getLogger("Task3.class");
 
     @BeforeMethod
     public void setup() {
         System.out.println("-------------Task 4------------");
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver = new ChromeDriver();
+        log.info("Launching Browser");
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
 
         driver.get("https://agoldoffish.wordpress.com/criminal-minds-opening-and-closing-quotes/");
+        log.info("Opening URL");
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
@@ -32,7 +34,7 @@ public class Task4 {
         System.out.println("Running Tests");
         List<WebElement> quotesGideon = driver.findElements(By.xpath("//p/strong[text()='Gideon']/ancestor::p"));
         int quotes_size = quotesGideon.size();
-
+        log.info("Reading all the Gideon quotes");
         String[] quotes = new String[quotes_size];
         for (int i = 0; i < quotes_size; i++) {
             quotes[i] = quotesGideon.get(i).getText();
@@ -44,9 +46,11 @@ public class Task4 {
         ArrayList<String> al = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(al.get(1));
         driver.get("https://www-5eb9089a8e101d4880a5fbda.recruit.eb7.io/");
+        log.info("Opening ebot7 web application");
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
+        log.info("Writing all the quotes");
         for (int i = 0; i < quotes_size; i++) {
 
             String[] arr = quotes[i].split("Gideon: ");
@@ -58,12 +62,14 @@ public class Task4 {
 
         }
 
+        log.info("Closing Child Window");
         driver.close();
         driver.switchTo().window(al.get(0));
     }
 
     @AfterMethod
     public void tearDown(){
+        log.info("Closing Parent Window");
         driver.close();
     }
 }
